b2e4e8d3ea609d7b41cfde97870bad115af59163
Lukas Kopenec <lukas@salsitasoft.com>
2019-04-29 11:03:40 +0200

    Enable compression on server.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import * as Koa from 'koa';
 import * as BodyParser from 'koa-bodyparser';
 import * as Router from 'koa-router';
 import * as Static from 'koa-static';
+import * as Compress from 'koa-compress';
 
 import apiRouter from './api';
 import { errorHandler } from './helpers/middlewares';
@@ -16,6 +17,7 @@ router.use('/api', apiRouter.routes(), apiRouter.allowedMethods());
 
 logger.info(`Application files are at ${APP_PATH}`);
 
+app.use(Compress());
 app.use(koaMiddleware());
 app.use(Static(APP_PATH));
 app.use(BodyParser());
