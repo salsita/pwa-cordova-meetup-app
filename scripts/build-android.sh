1c50fd381eb059a8e823695bbc5c2db5030e21a7
Jan Kotrlik <janko@salsitasoft.com>
2019-05-14 01:35:59 +0200

    Silent unzip, show cordova

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -16,8 +16,8 @@ else
   touch repositories.cfg
   curl -s https://dl.google.com/android/repository/$CURRENT > tools.zip
   curl -L -s https://dl.google.com/android/repository/platform-tools-latest-darwin.zip > platform-tools.zip
-  unzip tools.zip > /dev/null
-  unzip platform-tools.zip > /dev/null
+  unzip tools.zip &> /dev/null
+  unzip platform-tools.zip &> /dev/null
   rm platform-tools.zip tools.zip
   echo y | ./tools/bin/sdkmanager "$PLATFORM"
   echo y | ./tools/bin/sdkmanager "$BUILD_TOOLS"
