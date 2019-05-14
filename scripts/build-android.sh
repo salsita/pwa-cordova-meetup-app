368b2f71eaee7376cdb8b515c551c99cec7d2f65
Jan Kotrlik <janko@salsitasoft.com>
2019-05-14 02:02:24 +0200

    Better cache now

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -5,23 +5,5 @@ set -euo pipefail
 export PATH="$HOME/.android/tools:$HOME/.android/platform-tools:$PATH"
 export ANDROID_HOME="$HOME/.android"
 
-. ./android-tools-versions.env
-
-if [[ -d ~/.android ]] && [[ -d ~/.gradle ]] ; then
-  echo "Android SDK restored from cache"
-else
-  cd ~
-  mkdir .android
-  cd .android
-  touch repositories.cfg
-  curl -s https://dl.google.com/android/repository/$CURRENT > tools.zip
-  curl -L -s https://dl.google.com/android/repository/platform-tools-latest-darwin.zip > platform-tools.zip
-  unzip tools.zip &> /dev/null
-  unzip platform-tools.zip &> /dev/null
-  rm platform-tools.zip tools.zip
-  echo y | ./tools/bin/sdkmanager "$PLATFORM"
-  echo y | ./tools/bin/sdkmanager "$BUILD_TOOLS"
-fi
-
 cd /Users/distiller/app/cordova
 cordova build android --release --buildConfig build-android.json -- --versionCode="$CIRCLE_BUILD_NUM"
---
+++ scripts/install-android-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ -d ~/.android ]] && [[ -d ~/.gradle ]] ; then
+  echo "Android SDK restored from cache"
+  exit 0
+fi
+
+. ./android-tools-versions.env
+
+cd ~
+mkdir .android
+cd .android
+touch repositories.cfg
+curl -s https://dl.google.com/android/repository/$CURRENT > tools.zip
+curl -L -s https://dl.google.com/android/repository/platform-tools-latest-darwin.zip > platform-tools.zip
+unzip tools.zip &> /dev/null
+unzip platform-tools.zip &> /dev/null
+rm platform-tools.zip tools.zip
+echo y | ./tools/bin/sdkmanager "$PLATFORM"
+echo y | ./tools/bin/sdkmanager "$BUILD_TOOLS"
